67c66c7942cf2adcbdfdd8c98d69581bcf9210b4
dalia <dalia.hamza02@gmail.com>
2022-12-19 14:33:12 +0200

    version1.1

--- app/src/main/java/com/example/myproject/SignUp.java
+++ app/src/main/java/com/example/myproject/SignUp.java
@@ -5,12 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
 
     private EditText user;
     private EditText password;
@@ -22,21 +19,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        user =  findViewById(R.id.user);
-        password = findViewById(R.id.password);
-        email = findViewById(R.id.email);
-        number = findViewById(R.id.number);
-        numid= findViewById(R.id.numid);
+        setContentView(R.layout.activity_welcome);
+
     }
 
     public void signup(View view) {
-        Intent in = new Intent(this,MainActivity.class);
+
+        Intent in = new Intent(this, SignUp.class);
         startActivity(in);
     }
 
     public void login(View view) {
-        Intent in = new Intent(this,LogIn.class);
+        Intent in = new Intent(this, LogIn.class);
         startActivity(in);
     }
 }
